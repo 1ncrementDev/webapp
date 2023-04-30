554c9d7560e4295ac3e677be402420539fa819a0
Danil1110 <daniilscherbakov1337@gmail.com>
2023-05-01 00:28:11 +0300

    update

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ bot = telebot.TeleBot('6273729472:AAFl8MBcej7N8ccfSEM8dlg7bSltfhh67aY')
 def send_welcome(message):
     # Отправляем приветственное сообщение и кнопку
     reply_markup = types.InlineKeyboardMarkup()
-    url_button1 = types.InlineKeyboardButton("Онлайн магазин", web_app=types.WebAppInfo('https://1ncrementdev.github.io/webapp/'))
+    url_button1 = types.InlineKeyboardButton("Угадай Число", web_app=types.WebAppInfo('https://1ncrementdev.github.io/webapp/'))
     reply_markup.add(url_button1)
-    bot.reply_to(message, "➣ Добро пожаловать в магазин! \n➢ Нажмите на любую кнопку ниже, чтобы перейти к Веб-Приложению магазина.", reply_markup=reply_markup)
+    bot.reply_to(message, '➣ Добро пожаловать в игру "Угадай Число"! \n➢ Нажмите на любую кнопку ниже, чтобы перейти к Веб-Приложению.', reply_markup=reply_markup)
 
 
 bot.polling()
