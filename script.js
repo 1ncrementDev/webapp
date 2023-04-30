16b251a8c44893405d1064d521ec8db4c68770db
Danil1110 <daniilscherbakov1337@gmail.com>
2023-04-30 18:25:01 +0300

    update

--- script.js
+++ script.js
@@ -4,7 +4,13 @@ function handleData(data) {
   document.getElementById('price').textContent = `$${toncoinPrice.toFixed(2)}`;
 }
 
-// Создание скрипта для загрузки данных из API coinmarketcap.com
-const script = document.createElement('script');
-script.src = 'https://coinmarketcap.com/currencies/toncoin/';
-document.head.appendChild(script);
+// Функция для загрузки данных из API coinmarketcap.com и обновления цены TONcoin
+function updatePrice() {
+  const script = document.createElement('script');
+  script.src = 'https://coinmarketcap.com/currencies/toncoin/';
+  document.head.appendChild(script);
+}
+
+// Добавляем обработчик события для кнопки "Узнать цену TONcoin"
+const updateButton = document.getElementById('update-price');
+updateButton.addEventListener('click', updatePrice);
