ed4ffacd718a36db8ee276537091712eff9f7d83
1ncrementDev <131054819+1ncrementDev@users.noreply.github.com>
2023-05-01 02:07:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ bot = telebot.TeleBot('secret')
 def send_welcome(message):
     # Отправляем приветственное сообщение и кнопку
     reply_markup = types.InlineKeyboardMarkup()
-    url_button1 = types.InlineKeyboardButton("Угадай Число", web_app=types.WebAppInfo('https://1ncrementdev.github.io/webapp/'))
+    url_button1 = types.InlineKeyboardButton("Угадай Число", web_app=types.WebAppInfo('https://***'))
     reply_markup.add(url_button1)
     bot.reply_to(message, '➣ Добро пожаловать!\n\n➢ Нажмите на кнопку ниже, чтобы перейти к Веб-Приложению.', reply_markup=reply_markup)
 
